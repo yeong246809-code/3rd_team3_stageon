f9c23d6e71b57c92eec03961e9cddaf67c66128e
yeong246809 <yeong246809@gamil.com>
2026-07-28 20:46:00 +0900

    last chance

--- src/test/java/kr/co/stageon/ApplicationTests.java
+++ src/test/java/kr/co/stageon/ApplicationTests.java
@@ -2,9 +2,14 @@ package kr.co.stageon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ApplicationTests {
 
+    @Test
+    void contextLoads() {
+    }
 
 }
